8b5137504c8bdd08af05b4dad3b34ee25804873e
Aak Zaki <58508162+aak-zaki@users.noreply.github.com>
2025-07-12 12:42:37 +0700

    notifier.py

--- notifier.py
+++ notifier.py
@@ -1,2 +1,14 @@
 import requests
 from config import TELEGRAM_BOT_TOKEN, TELEGRAM_CHAT_ID
+
+def send_telegram(message):
+    url = f"https://api.telegram.org/bot{TELEGRAM_BOT_TOKEN}/sendMessage"
+    payload = {
+        "chat_id": TELEGRAM_CHAT_ID,
+        "text": message,
+        "parse_mode": "HTML"
+    }
+    try:
+        requests.post(url, json=payload, timeout=10)
+    except Exception as e:
+        print(f"Telegram error: {e}")
