abc45abb6c8c11eb2a6f72a6e8023a48bf52de50
Aak Zaki <58508162+aak-zaki@users.noreply.github.com>
2025-07-12 14:22:29 +0700

    config.py

--- config.py
+++ config.py
@@ -1,3 +1,2 @@
-TELEGRAM_BOT_TOKEN = "your_telegram_bot_token"
-TELEGRAM_CHAT_ID = "your_chat_id"
-GATE_API_URL = "https://api.gateio.ws/api/v4/spot/candlesticks"
+BOT_TOKEN = "your_telegram_bot_token"
+CHAT_ID = "your_chat_id"
